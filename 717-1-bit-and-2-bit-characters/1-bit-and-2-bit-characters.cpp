aa96ba31c69f58b30ecb5618af8ae1beff71e14f
coderx <76726175+kovid-sharma@users.noreply.github.com>
2025-11-19 12:09:17 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (71.46%) - LeetSync

---
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        for(int i=0;i<bits.size();i++)
+        {
+            if(i==bits.size()){return false;}
+            else if(i==bits.size()-1)return true;
+            if(bits[i]){
+                i++;
+            }
+            else{
+
+            }
+        }
+        return false;
+    }
+};
