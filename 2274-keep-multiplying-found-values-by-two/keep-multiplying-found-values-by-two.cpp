4e98d52dc37da336b97b5b38e078c8bba2800dd1
coderx <76726175+kovid-sharma@users.noreply.github.com>
2025-11-19 12:12:19 +0530

    Time: 7 ms (14.27%) | Memory: 16.1 MB (6.93%) - LeetSync

---
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        map<int,int>mp;
+        for(auto it:nums)mp[it]++;
+        while(mp.find(original)!=mp.end())
+            original*=2;
+        return original;
+    }
+};
