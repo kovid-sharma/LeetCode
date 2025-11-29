9b171cc4278f91dc54cc6ada7dab21386fb71ac7
coderx <76726175+kovid-sharma@users.noreply.github.com>
2025-11-29 20:18:14 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (35.30%) - LeetSync

---
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int sum = 0;
+        for(auto it:nums)sum+=it;
+
+        int rem = sum%k;
+        return rem;
+    }
+};
