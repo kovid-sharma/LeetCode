ebbfb85d951d5d97a8edf2692f96e48fd6bc9466
coderx <76726175+kovid-sharma@users.noreply.github.com>
2026-01-02 23:40:34 +0530

    Time: 3 ms (67.83%) | Memory: 15.1 MB (10.73%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -5,10 +5,8 @@ public:
         for(int i=0;i<nums.size();i++)
         {
             if(mp.find(target-nums[i])!=mp.end())
-            {
                 return {mp[target-nums[i]],i};
-            }
-            mp[nums[i]]=i;
+            mp[nums[i]]=i;    
         }
         return {-1,-1};
     }
