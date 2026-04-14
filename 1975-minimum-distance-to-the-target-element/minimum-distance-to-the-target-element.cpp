a4ca01272fd333d8eeeae93c9e97f70818856978
coderx <76726175+kovid-sharma@users.noreply.github.com>
2026-04-14 08:26:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (84.22%) - LeetSync

---
+++ 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int minD = INT_MAX;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==target)
+            {
+                minD = min(minD, abs(i-start));
+            }
+        }
+        return minD;
+        
+    }
+};
