1742137579ba21b30e5a404c6915e7528128a3ce
coderx <76726175+kovid-sharma@users.noreply.github.com>
2026-09-02 08:18:16 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (36.43%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // odd - odd = even. // odds 
+        // even - even = even // evens
+        // any other combo = odd 
+        return true;
+
+    }
+};
