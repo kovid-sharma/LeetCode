2ccb8e73df8b12a0deb314ed717b8f9119131c99
coderx <76726175+kovid-sharma@users.noreply.github.com>
2026-09-05 09:26:21 +0530

    Time: 20 ms (22.40%) | Memory: 210.3 MB (67.05%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -2,26 +2,19 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int>minTillNow;
-        vector<int>maxTillNow;
+        vector<pair<int,int>>stableInfo(n);
         int mx = INT_MIN;
         int mi = INT_MAX;
         for(int i=0;i<n;i++)
         {
             mx = max(mx,nums[i]);
-            maxTillNow.push_back(mx);
+            mi = min(mi,nums[n-1-i]);
+            stableInfo[i].first = mx;
+            stableInfo[n-1-i].second = mi;
         }
-
-        for(int i=n-1;i>=0;i--)
-        {
-            mi = min(mi,nums[i]);
-            minTillNow.push_back(mi);
-        }
-
-
         for(int i=0;i<n;i++)
         {
-            int stableScore = maxTillNow[i] - minTillNow[n-i-1];
+            int stableScore = stableInfo[i].first - stableInfo[i].second;
             if(stableScore <= k) return i;
         }
         return -1;
