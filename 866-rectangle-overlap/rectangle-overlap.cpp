7e332d0b367982221c817f71f3119154d4f861fc
coderx <76726175+kovid-sharma@users.noreply.github.com>
2026-09-14 08:45:05 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (11.03%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        
+        // x axis
+        return rec1[0] < rec2[2] && rec1[2]>rec2[0] && 
+               rec1[1] < rec2[3] && rec1[3]>rec2[1];
+    }
+
+
+
+    // a,b
+    // c,d
+
+
+    // b>c && a<d
+};
